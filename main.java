f08a13ee0024f02c7988c040186075b94008400c
MALAR21 <43902205+MALAR21@users.noreply.github.com>
2019-02-12 10:10:34 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,34 +1,31 @@
-import java.util.Scanner;
-class Main
+class Char
 {
-   public static void main(String[ ] arg)
-   {
-	boolean isVowel=false;;
-	Scanner scanner=new Scanner(System.in);
+	public static void main(String[ ] arg)
+	{
+	int i=0;
+	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter a character : ");
-	char ch=scanner.next().charAt(0); 
-	scanner.close();
+	char ch=sc.next( ).charAt(0);	 
+	//char ch=sc.nextChar();
 	switch(ch)
 	{
-	   case 'a' :
-	   case 'e' :
-    	   case 'i' :
-	   case 'o' :
-	   case 'u' :
-	   case 'A' :
-	   case 'E' :
-	   case 'I' :
-	   case 'O' :
-	   case 'U' : isVowel = true;
+	 case  'a' :
+	 case 'e'  :
+	 case 'i'   :
+	 case 'o'  :
+	 case 'u'  :
+	 case  'A' :
+	 case 'E'  :
+	 case 'I'   :
+	 case 'O'  :
+	 case 'U'  :i++;
 	}
-	if(isVowel == true) {
-	   System.out.println(ch+" is  a Vowel");
+	if(i==1)
+	System.out.println("Entered character "+ch+" is  Vowel");
+	else 
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Entered character "+ch+" is Consonent");
+		else
+		System.out.println("Not an alphabet");		
 	}
-	else {
-	   if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-		System.out.println(ch+" is a Consonant");
-	   else
-		System.out.println("Input is not an alphabet");		
-        }
-   }
 }
